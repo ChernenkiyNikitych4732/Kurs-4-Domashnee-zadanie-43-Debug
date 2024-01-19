c807fef2e3912eaf3f028e7beae19c1936483556
ChernenkiyNikitych4732 <chernenkiy.nikitychsssr@gmail.com>
2024-01-19 18:44:07 +0100

    Курс 4 Домашнее задание 43 Debug

---
+++ src/main/java/skypro/javaunion/teamdevelopmenttelegrambot/listener/TelegramBotUpdateListener.java
@@ -0,0 +1,13 @@
+package skypro.javaunion.teamdevelopmenttelegrambot.listener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import skypro.javaunion.teamdevelopmenttelegrambot.TelegramBotApplication;
+
+@SpringBootApplication
+public class TelegramBotUpdateListener {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotApplication.class);
+    }
+}
